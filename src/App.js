021a2448123aecd7e48eaaa1d761c0fd0bf05841
Casey Olson <casey@bookcasey.com>
2022-11-14 09:43:54 -0600

    morning lecture demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section className="App">
+        <h1>Casey's Cool Web App</h1>
+        <p>Welcome to my site!</p>
+        <p>Hello hello</p>
+        <div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </section>
+      <div></div>
+    </React.Fragment>
   );
 }
 
